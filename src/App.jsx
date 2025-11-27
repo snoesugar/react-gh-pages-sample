3e68cf4b91aa991ac43828fdb699c5ba29e91993
happy <daisy831015@gmail.com>
2025-11-27 13:02:31 +0800

    使用bootstrap function

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import axios from 'axios'
+import { Modal } from 'bootstrap'
 
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -7,21 +8,42 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const modalRef = useRef(null)
+  const customModal = useRef(null)
 
   useEffect(() => {
     (async () => {
       const res = await axios.get('https://randomuser.me/api/')
       console.log(res);
+      openModal()
+
+      setTimeout(() => {
+        closeModal()
+      }, 2000);
     }) ()
   }, [])
 
+  useEffect(() => {
+    //console.log(modalRef.current);
+    customModal.current = new Modal(modalRef.current);
+    //customModal.current.show()
+  },[])
+
+  const openModal = () => {
+    customModal.current.show()
+  }
+
+  const closeModal = () => {
+    customModal.current.hide()
+  }
+
   return (
     <>
-<button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+<button type="button" className="btn btn-primary" onClick={() => openModal()}>
   Launch demo modal
 </button>
 
-<div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div className="modal fade" ref={modalRef} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div className="modal-dialog">
     <div className="modal-content">
       <div className="modal-header">
